53431736d6bceb37627b1a4f4e6cb08ad9599a1e
Andrea Evelyn Mejía Rubio <andiemartin33@gmail.com>
2021-04-13 10:35:16 -0500

    Agregar archivos de proyecto.

---
+++ Hilos_mutex/Operaciones.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace Hilos_mutex
+{
+
+    class Operaciones
+    {
+         private int v1, v2, resultado;
+         private static Mutex mut = new Mutex();
+
+    public Operaciones(int valor1, int valor2)
+        {
+
+            v1 = valor1;
+            v2 = valor2;
+
+        }
+
+        public void Sumar()
+        {
+            mut.WaitOne();
+            Console.WriteLine("{0} Ingresando Codigo Protegido",Thread.CurrentThread.Name);
+            resultado = v1 + v2;
+            Thread.Sleep(1000);
+            Console.WriteLine("El resultado de la suma es: "+ resultado);
+            Console.WriteLine("{0} Saliendo de codigo protegido",Thread.CurrentThread.Name);
+            Console.WriteLine(" ");
+            mut.ReleaseMutex();
+
+        }
+
+        public void Restar()
+        {
+
+            mut.WaitOne();
+            Console.WriteLine("{0} Ingresando Codigo Protegido", Thread.CurrentThread.Name);
+            resultado = v1 - v2;
+            Thread.Sleep(1000);
+            Console.WriteLine("El resultado de la resta es: " + resultado);
+            Console.WriteLine("{0} Saliendo de codigo protegido", Thread.CurrentThread.Name);
+            Console.WriteLine(" ");
+            mut.ReleaseMutex();
+
+        }
+
+
+        public void Multiplicar()
+        {
+
+            mut.WaitOne();
+            Console.WriteLine("{0} Ingresando Codigo Protegido", Thread.CurrentThread.Name);
+            resultado = v1 * v2;
+            Thread.Sleep(1000);
+            Console.WriteLine("El resultado de la multiplicacion es: " + resultado);
+            Console.WriteLine("{0} Saliendo de codigo protegido", Thread.CurrentThread.Name);
+            Console.WriteLine(" ");
+
+           mut.ReleaseMutex();
+
+        }
+
+
+
+
+    }
+}
---
+++ Hilos_mutex/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace Hilos_mutex
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Operaciones objop = new Operaciones(5, 7);
+            //creacion de hilos
+            Thread H1 = new Thread(new ThreadStart(objop.Sumar));
+            H1.Name = string.Format("Hilo 1");
+
+            Thread H2 = new Thread(new ThreadStart(objop.Restar));
+            H2.Name = string.Format("Hilo 2");
+
+            Thread H3 = new Thread(new ThreadStart(objop.Multiplicar));
+            H3.Name = string.Format("Hilo 3");
+
+
+            //inicio de ejecucion de cada hilo
+
+            H1.Start() ;
+            H2.Start();
+            H3.Start();
+
+
+
+
+        }
+    }
+}
